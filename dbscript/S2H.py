e7473cb5ac620e1ace753f6a065d31c0e466ba7c
shouldsee <shouldsee.gem@gmail.com>
2017-08-05 18:27:31 +0100

    Added database_manipulation scripts

---
+++ dbscript/S2H.py
@@ -0,0 +1,28 @@
+from tst.utils_db import *
+from time import time
+
+v_curr = verify_version('putative')
+
+#### Calculate superfamily-based statistics
+
+t0 = time()
+
+
+level_curr = level.objects.get(letter = 'H')
+# node_set =  classification.objects.filter(version = v_curr)
+node_set =  classification.objects.filter(version__id__lte = v_curr.id)
+lst = node_set.filter(level = level_curr)
+
+print("calculating superfamily stats for %d from verion: %s" % (len(lst), v_curr.name))
+c = counter(lst, per = 100)
+failcount = 0
+for d in lst:
+    c.count()
+    with transaction.atomic():
+#     if 1:
+        failcount += homsf_stat_fill(d) == 0
+
+print '%d instances failed' % failcount
+print('Ended after %.4f' % (time()-t0))  # len(lst)
+# homsf_stat_fill(lst[0])
+# print(str(lst[0]))
