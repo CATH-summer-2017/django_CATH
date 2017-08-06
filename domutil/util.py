4d667547b4ab88d766be56c23494c1e5ad10832d
shouldsee <shouldsee.gem@gmail.com>
2017-08-06 20:15:18 +0100

    refactor domutil to separate modeller

--- domutil/util.py
+++ domutil/util.py
@@ -1,7 +1,7 @@
-from modeller import *
-from modeller.scripts import complete_pdb
-### Enable this line to reduce verbositys
-# log.none()
+# from modeller import *
+# from modeller.scripts import complete_pdb
+# ### Enable this line to reduce verbositys
+# # log.none()
 import sys
 import StringIO
 import contextlib
@@ -46,59 +46,6 @@ class counter():
             print('%d of %d'%(self.i,self.imax))
         self.i += 1
 
-def get_something( pdbfile, env = None, auto_complete = False, s0 = None):
-	if not env:
-		env = environ()
-		#env.io.atom_files_directory = ['../atom_files']
-		env.io.atom_files_directory = ['../pdbs']
-		env.libs.topology.read(file='$(LIB)/top_heav.lib')
-		env.libs.parameters.read(file='$(LIB)/par.lib')
-	if auto_complete:
-		mdl = complete_pdb(env, pdbfile)
-	else:
-		mdl = model(env)
-		mdl.read( pdbfile, model_format='PDB', model_segment=('FIRST:@', 'LAST:'), io=None);
-	if not s0:
-		s0 = StringIO.StringIO();
-	else:
-		s0.truncate(0)
-	
-	with stdoutIO(s0) as s:
-		nDOPE = mdl.assess_normalized_dope()
-	s0buf = ''.join(s0.buflist)
-	# return 
-	outdict = {"nDOPE":nDOPE,
-		"DOPE": float(p_energy.findall(s0buf)[0]),
-		"nbpair_count":int(p_nb.findall(s0buf)[0]),
-		"atom_count":int(p_atomCount.findall(s0buf)[0].strip()),
-		 "res_count":  int(p_resCount.findall(s0buf)[0]),
-		  }
-	return outdict
-
-
-def get_nDOPE( pdbfile, env = None, auto_complete = False, **kwargs):
-# pdbfile = "4xz8A_chop"
-#mdl = complete_pdb(env, "1fas")
-	
-	### Use existing environment to avoid redundant re-initialisation.
-	# if not env:
-	# 	env = environ()
-	# 	#env.io.atom_files_directory = ['../atom_files']
-	# 	env.io.atom_files_directory = ['../pdbs']
-	# 	env.libs.topology.read(file='$(LIB)/top_heav.lib')
-	# 	env.libs.parameters.read(file='$(LIB)/par.lib')
-
-	# if auto_complete:
-	# 	mdl = complete_pdb(env, pdbfile)
-	# else:
-	# 	mdl = model(env)
-	# 	mdl.read( pdbfile, model_format='PDB', model_segment=('FIRST:@', 'LAST:'), io=None);
-	# nDOPE = mdl.assess_normalized_dope()
-	# # pdbname = os.path.basename(pdbfile)
-
-
-	nDOPE = get_something(pdbfile, env, auto_complete,**kwargs).get("nDOPE")
-	return nDOPE
 
 import csv
 
