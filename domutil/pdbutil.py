5ae9460d677949e569e25099d04820b90d4d1bc9
shouldsee <shouldsee.gem@gmail.com>
2017-08-07 14:57:56 +0100

    new rou

--- domutil/pdbutil.py
+++ domutil/pdbutil.py
@@ -1,38 +1,85 @@
-from modeller import *
-from modeller.scripts import complete_pdb
 ### Enable this line to reduce verbositys
 # log.none()
+from Bio.PDB import *
 from .util import *
+import sys,os
 
-
-def get_something( pdbfile, env = None, auto_complete = False, s0 = None):
-	if not env:
-		env = environ()
-		#env.io.atom_files_directory = ['../atom_files']
-		env.io.atom_files_directory = ['../pdbs']
-		env.libs.topology.read(file='$(LIB)/top_heav.lib')
-		env.libs.parameters.read(file='$(LIB)/par.lib')
-	if auto_complete:
-		mdl = complete_pdb(env, pdbfile)
+def parse_PDB(pdbname,pdbdir=None,parser = None):
+	if pdbdir:
+		pass
 	else:
-		mdl = model(env)
-		mdl.read( pdbfile, model_format='PDB', model_segment=('FIRST:@', 'LAST:'), io=None);
-	if not s0:
-		s0 = StringIO.StringIO();
-	else:
-		s0.truncate(0)
+		if 'PDBlib' in os.environ.keys():
+			pdbdir = '$PDBlib/'
+		else:
+			os.environ['$PDBlib'] = './'
+			pdbdir = '$PDBlib/'
+	pdbfile = os.path.expandvar(pdbdir) + pdbname
 	
-	with stdoutIO(s0) as s:
-		nDOPE = mdl.assess_normalized_dope()
-	s0buf = ''.join(s0.buflist)
-	# return 
-	outdict = {"nDOPE":nDOPE,
-		"DOPE": float(p_energy.findall(s0buf)[0]),
-		"nbpair_count":int(p_nb.findall(s0buf)[0]),
-		"atom_count":int(p_atomCount.findall(s0buf)[0].strip()),
-		 "res_count":  int(p_resCount.findall(s0buf)[0]),
+	if not parser:
+		parser = PDBParser()
+	struct = parser.get_structure('X', pdbfile)
+
+	return struct
+
+def get_something(pdbname, env = None, auto_complete = False, s0 = None, pdbdir = None, cutout=15.0,cutin=3.5):
+
+	struct = parse_PDB(pdbname)
+	# struct = p.get_structure('X', pdbfile)
+	alst = list(struct.get_atoms())
+	rcount = sum( 1 for _ in struct.get_residues())
+	nbpair_count = (
+	len( NeighborSearch(alst ).search_all(radius =cutout))  
+	-  len( NeighborSearch(alst ).search_all(radius =cutin)) 
+	)
+
+	outdict = {"nDOPE":0,
+		"DOPE": 0,
+		"nbpair_count":nbpair_count,
+		"atom_count": len(alst),
+		 "res_count": rcount,
 		  }
-	return outdict
+
+
+# if django.settings
+
+if 'USE_MODELLER' in os.environ.keys():
+	USE_MODELLER = int(os.environ['USE_MODELLER'])
+else:
+	USE_MODELLER = 0
+if USE_MODELLER:
+	from modeller import *
+	from modeller.scripts import complete_pdb
+
+	def get_something( pdbfile, env = None, auto_complete = False, s0 = None):
+		if not env:
+			env = environ()
+			#env.io.atom_files_directory = ['../atom_files']
+			env.io.atom_files_directory = ['../pdbs',
+			'$(PDBlib)']
+			env.libs.topology.read(file='$(LIB)/top_heav.lib')
+			env.libs.parameters.read(file='$(LIB)/par.lib')
+		if auto_complete:
+			mdl = complete_pdb(env, pdbfile)
+		else:
+			mdl = model(env)
+			mdl.read( pdbfile, model_format='PDB', model_segment=('FIRST:@', 'LAST:'), io=None);
+		
+		if not s0:
+			s0 = StringIO.StringIO();
+		else:
+			s0.truncate(0)
+		
+		with stdoutIO(s0) as s:
+			nDOPE = mdl.assess_normalized_dope()
+		s0buf = ''.join(s0.buflist)
+		# return 
+		outdict = {"nDOPE":nDOPE,
+			"DOPE": float(p_energy.findall(s0buf)[0]),
+			"nbpair_count":int(p_nb.findall(s0buf)[0]),
+			"atom_count":int(p_atomCount.findall(s0buf)[0].strip()),
+			 "res_count":  int(p_resCount.findall(s0buf)[0]),
+			  }
+		return outdict
 
 
 def get_nDOPE( pdbfile, env = None, auto_complete = False, **kwargs):
