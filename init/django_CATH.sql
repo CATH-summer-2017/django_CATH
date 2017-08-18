d9e7cf484a18a785dd6cebe2af38f82add0d6096
Feng Geng <shouldsee@github.com>
2017-08-18 16:37:16 +0100

    commenting the SQL statements

--- init/django_CATH.sql
+++ init/django_CATH.sql
@@ -1,10 +1,25 @@
+
+
+#### This CREATEs a new MySQL user named "django" with password as "Django_passw0rd"
+CREATE USER "django"@"localhost" IDENTIFIED BY "Django_passw0rd";
+
+#### This CREATEs a separate SQL DATABASE named "django"
+CREATE DATABASE django CHARACTER SET utf8;  
+
+#### This GRANTs appropriate privileges to the user "django"@"localhost"
+GRANT ALL PRIVILEGES on django.* TO "django"@"localhost" ;
+
+
+
+
+#### CREATE more DATABASEs and GRANT correct privileges
+CREATE DATABASE mtest_django character set utf8;
+GRANT ALL PRIVILEGES on mtest_django.* to 'django'@'localhost';
+
+CREATE DATABASE test_django character set utf8;
+GRANT ALL PRIVILEGES on test_django.* to 'django'@'localhost';
+
+
+
+##### Alternatively you can grant all privileges to "django" (not safe)
 -- GRANT ALL PRIVILEGES ON * . * TO 'django'@'localhost';
-
-create database django character set utf8;
-grant all privileges on django.* to 'django'@'localhost' identified by 'Django_passw0rd';
-
-create database mtest_django character set utf8;
-grant all privileges on mtest_django.* to 'django'@'localhost' identified by 'Django_passw0rd';
-
-create database test_django character set utf8;
-grant all privileges on test_django.* to 'django'@'localhost' identified by 'Django_passw0rd';
