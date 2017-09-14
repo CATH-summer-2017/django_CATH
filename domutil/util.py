22125ab30278cc58a451617c904222b0abe9b009
shouldsee <shouldsee.gem@gmail.com>
2017-09-14 21:37:25 +0100

    added getattribute() from templatetags, added assert_expr() for easier assertion

--- domutil/util.py
+++ domutil/util.py
@@ -663,3 +663,75 @@ def wrap_mat(Da,Db):
     OUTPUT.update(d)
     c.summary("zipping sparse matrix")
     return OUTPUT
+
+
+
+
+TEMPLATE_STRING_IF_INVALID = 'No attr:'
+
+numeric_test = re.compile("^\d+$")
+def getattribute_none(value, arg):
+    """Gets an attribute of an object dynamically from a string name"""
+    if hasattr(value, str(arg)):
+        attr = getattr(value, str(arg));
+        if callable(attr):
+            return attr();
+        else:
+            return attr
+    elif hasattr(value, 'has_key') and value.has_key(arg):
+        return value[arg]
+    elif numeric_test.match(str(arg)) and len(value) > int(arg):
+        return value[int(arg)]
+    else:
+        return 'None'
+
+def getattribute(value, arg):
+    """Gets an attribute of an object dynamically from a string name"""
+    if arg.endswith("?"):
+        arg = arg[:-1]
+        callit = 1
+    else:
+        callit = 0
+
+    if hasattr(value, str(arg)):
+        attr = getattr(value, str(arg));
+        # tp = type(attr) 
+        # if callable(attr) and tp != "django.db.models.manager.Manager":
+        if callit:
+            return attr();
+        else:
+            return attr
+    elif hasattr(value, 'has_key') and value.has_key(arg):
+        return value[arg]
+    elif numeric_test.match(str(arg)) and len(value) > int(arg):
+        return value[int(arg)]
+    else:
+        return TEMPLATE_STRING_IF_INVALID + ' ' + arg
+
+def getattribute_iter(value, args):
+    args = args.split('__');
+    # value 
+    for arg in args:
+        if arg:
+            value = getattribute(value,arg);
+        else:
+            pass
+
+    return value
+
+
+def assert_expr(act, expr = '', behave = None, **kwargs):
+    #### remember to pass your data_dict in kwargs, especially what got mentioned in "expr" ###########
+    #######################################################################
+    ####  example: expr = " == exp "  ==>  fullexpr = "act == exp"
+    ####           "exp" needs to be passed as a keyword argument assert_expr(... , exp = "12345") 
+    ###################################################################################
+#     expr0 = expr[:]
+    if not expr:
+        expr = '== True '
+        
+    fullexpr = ' act ' + expr
+    basemsg = "Expecting:%s, Actual:%s " % (expr,act)
+    success = eval(fullexpr,locals(),kwargs)
+    assert( success ),"[FAILED]:" + basemsg
+    print "[PASSED]:" + basemsg
