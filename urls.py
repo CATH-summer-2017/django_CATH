5f02ef78321c78a725d2fb132c78864d730b3967
shouldsee <shouldsee.gem@gmail.com>
2017-09-15 09:58:20 +0100

    cleaning

--- urls.py
+++ urls.py
@@ -29,38 +29,20 @@ urlpatterns = [
     url(r'^CCXhit/$', views.test__CCXhit, name='CCXhit_handler'),
 
 
-    # ex: /tst/5/
-    # url(r'^(?P<question_id>[0-9]+)/$', 
-    # 	views.detail,
-    # 	name='detail'),
-    
-    # # ex: /tst/5/results/
-    # url(r'^(?P<question_id>[0-9]+)/results/$',
-    # 	views.results,
-    # 	name = 'results'),
-    
-    # # ex:/tst/5/vote/
-    # url(r'^(?P<question_id>[0-9]+)/vote/$',
-    # 	views.vote,
-    # 	name = 'vote'),
 
 
-    # ex:/tst/
+
+    # ex:/tst/domain
     url(r'^domain/$',
     	views.domain_collection,
-        # {}        
     	name = 'domain_collection'),
 
-    # ex:/tst/d1fpzD00/
+    # ex:/tst/domain/d1fpzD00/
     url(r'^domain/(?P<domain_id>[\d,a-z,A-Z]+)/$',
     	views.domain_detail,
     	name = 'domain_detail'),
 
-	# ex:/tst/superfamily/3.90.190.10/
-    # url(r'^superfamily/$',
-    #     views.homsf_s35_collection,
-    #     name = 'homsf_s35_collection'),
-    
+	# ex:/tst/superfamily/3.90.190.10/    
     url(r'^superfamily/$',
         views.homsf_collection,
         name = 'homsf_collection'),
